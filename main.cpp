1731e702b66c3b8d4cdbf6f16d69edcbea93473e
FiremelonX8 <twitteranotherbrick@gmail.com>
2026-05-17 21:57:39 -0300

    feat: add first part of the project (NOT TESTED)

---
+++ main.cpp
@@ -0,0 +1,184 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <stdexcept>
+#include "array_stack.h"
+
+using namespace std;
+
+
+class Cenario {
+  public:
+    Cenario(string& texto, size_t indice_inicial) {
+        size_t pos = indice_inicial;
+        nome = proxima_tag_conteudo(texto, pos, "nome");
+        altura = static_cast<size_t>( stoi( proxima_tag_conteudo(texto, pos, "altura") ) );
+        largura = static_cast<size_t>( stoi( proxima_tag_conteudo(texto, pos, "largura") ) );
+        x = static_cast<size_t>( stoi( proxima_tag_conteudo(texto, pos, "x") ) );
+        y = static_cast<size_t>( stoi( proxima_tag_conteudo(texto, pos, "y") ) );
+        matriz = matriz_remove_espacos( proxima_tag_conteudo(texto, pos, "matriz") );
+        indice_final = pos;
+    }
+    ~Cenario() {};
+    string nome;
+    size_t altura;
+    size_t largura;
+    size_t x;
+    size_t y;
+    string matriz;
+    size_t indice_final;
+
+  private:
+    string proxima_tag(string& texto, size_t& pos) {
+        string tag = "";
+        for ( ; pos < texto.length(); pos++) {
+            if (texto[pos] == '<') {
+                pos++;
+                while (texto[pos] != '>') {
+                    tag += texto[pos];
+                    pos++;
+                }
+                pos++;
+                return tag;
+            }
+        }
+        return tag;
+    }
+    string proximo_conteudo(string& texto, size_t& pos) {
+        string txt = "";
+        while (texto[pos] != '<') {
+            txt += texto[pos];
+            pos++;
+        }
+        while (texto[pos] != '>') {
+            pos++;
+        }
+        pos++;
+        return txt;
+    }
+    string proxima_tag_conteudo(string& texto, size_t& pos, string nome_tag) {
+        string tag = "";
+        while (tag != nome_tag) {
+            tag = proxima_tag(texto, pos);
+        }
+        return proximo_conteudo(texto, pos);
+    }
+    string matriz_remove_espacos(string texto) {
+        string saida;
+        for (int i = 0; i < texto.length(); i++) {
+            if (texto[i] == '0' || texto[i] == '1') {
+                saida += texto[i];
+            }
+        }
+        return saida;
+    }
+};
+
+// Verifies if the tag is a opening one or a closing one
+bool isOpeningTag(string& tagText) {
+    if (tagText[0] == '/') {
+        return false;
+    }
+    return true;
+}
+
+// Gets the text and returns the verification for the identified tags
+// TODO: REWRITE THIS FUNCTION AND DIVIDE IT INTO MORE CONCISE FUNCTIONS
+void verifyTag(string &texto) {
+    structures::ArrayStack<string> stackTags = structures::ArrayStack<string>(800);
+    bool isOpTag;
+    string tagText;
+
+    // For restoring the correct "next iteration" in for loop after while loop
+    int contTexto = 0;
+    for (int i = 0; i < texto.length(); i++) { 
+        if (texto[i] == '<') {
+            // While loop for storing the string that corresponds to the tag name
+            while (texto[i] != '>') {
+                tagText += texto[i];
+                // Adds one unit to each character iterated
+                contTexto++;
+            }
+            if (texto[i] != '>') {
+                throw runtime_error("The tag is incorrect:");
+            }
+        }
+        isOpTag = isOpeningTag(tagText);
+        if (isOpTag) {
+            stackTags.push(tagText);
+        } else {
+            // For erasing the '/' character in order to contain the real text 
+            // indicating the tag
+            tagText = tagText.erase(0);
+            if (stackTags.empty()) {
+                throw runtime_error("The hierarchy is incorrect: " + tagText + " is closing without previous opening");
+            }
+            if (tagText == stackTags.top()) {
+                stackTags.pop();
+            } else {
+                throw runtime_error("The hierarchy is incorrect: " + stackTags.top() + " is different from " + tagText);
+            }
+        }
+        i += contTexto;
+    }
+
+    if (!stackTags.empty()) {
+        throw runtime_error("The hierarchy is incorrect: a tag was not closed");
+    }
+}
+
+
+/**********************
+    FUNÇÃO PRINCIPAL
+***********************/
+int main() {
+
+    string filename;
+
+    std::cin >> filename;  // nome do arquivo de entrada 
+                           // (no 'executar': escrever pelo teclado;
+                           //  no 'avaliar' : nome é passado pelos testes)
+
+    // Abertura do arquivo
+    ifstream filexml(filename);
+    if (!filexml.is_open()) {
+        cerr << "Erro ao abrir o arquivo " << filename << endl;
+        throw runtime_error("Erro no arquivo XML");
+    }
+
+    // Leitura do XML completo para 'texto'
+    string texto;
+    char character;
+    while (filexml.get(character)) {
+        texto += character;
+    }
+
+
+    // ----------------------------
+    // Sugestão de código para a PARTE 2 do projeto
+
+    // Exemplo de leitura do primeiro cenário - REMOVER ESTAS SAÍDAS DE TELA NA VERSÃO FINAL
+    Cenario c1(texto, 0);
+
+    cout << "nome   : " << c1.nome << endl;
+    cout << "altura : " << c1.altura << endl;
+    cout << "largura: " << c1.largura << endl;
+    cout << "x      : " << c1.x << endl;
+    cout << "y      : " << c1.y << endl;
+    cout << "matriz : " << c1.matriz << endl << endl;
+
+    // Exemplo de leitura do segundo cenário (a partir do índice final de c1) - REMOVER ESTAS SAÍDAS DE TELA NA VERSÃO FINAL
+    Cenario c2(texto, c1.indice_final);
+
+    cout << "nome   : " << c2.nome << endl;
+    cout << "altura : " << c2.altura << endl;
+    cout << "largura: " << c2.largura << endl;
+    cout << "x      : " << c2.x << endl;
+    cout << "y      : " << c2.y << endl;
+    cout << "matriz : " << c2.matriz << endl << endl;
+
+    // OWN CODE:
+    verifyTag(texto);
+
+    return 0;
+}
